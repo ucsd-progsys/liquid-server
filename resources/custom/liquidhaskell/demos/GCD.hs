597513b9005e91b82aad9edff7996e726a53934b
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-01-20 17:59:28 -0800

    asdasdASD

--- resources/custom/liquidhaskell/demos/GCD.hs
+++ resources/custom/liquidhaskell/demos/GCD.hs
@@ -2,6 +2,25 @@ module GCD where
 
 import Prelude hiding (gcd, mod)
 
+
+fibOk  :: Int -> Int
+fibBad :: Int -> Int
+
+{-@ fibBad    :: Int -> Int @-}
+fibBad 0      = 1
+fibBad 1      = 1
+fibBad n      = fibBad (n-1) + fibBad (n-2)
+
+{-@ fibOk :: Nat -> Nat @-}
+fibOk n 
+  | n <= 1    = 1
+  | otherwise = fibOk (n-1) + fibOk (n-2)
+
+
+
+
+
+
 {-@ mod :: a:Nat -> b:{v:Nat| ((v < a) && (v > 0))} -> {v:Nat | v < b} @-}
 mod :: Int -> Int -> Int
 mod a b | a - b >  b = mod (a - b) b
