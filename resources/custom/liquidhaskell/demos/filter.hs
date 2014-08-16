c0b4a92d2acbbe23d9d35f7be8248e3ded5578b5
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-08-16 09:39:21 -0700

    ado

--- resources/custom/liquidhaskell/demos/filter.hs
+++ resources/custom/liquidhaskell/demos/filter.hs
@@ -1,46 +1,68 @@
-module Filter3 where
+{-@ LIQUID "--short-names" @-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
--- Dump plain sigs here.
+module Filter (filter) where
 
 import Prelude hiding (filter)
-isNat, isEven, isOdd :: Int -> Maybe Int
-filter :: (a -> Maybe a) -> [a] -> [a]
+import Data.Set (Set)
 
------------------------------------------------------------------
--- | Preliminaries 
------------------------------------------------------------------
+import Prelude hiding (filter)
+isPos, isEven, isOdd :: Int -> Maybe Int
+filter, filter3      :: (a -> Maybe a) -> [a] -> [a]
 
-{-  type Nat  = {v:Int | 0 <= v}        -}
-{-@ type Even = {v:Int | v mod 2 == 0} @-}
-{-@ type Odd  = {v:Int | v mod 2 /= 0} @-}
+{-@ measure elts :: [a] -> (Set a)
+    elts ([])   = {v | Set_emp v }
+    elts (x:xs) = {v | v = Set_cup (Set_sng x) (elts xs) }
+  @-}
 
-{-@ isNat          :: Int -> Maybe Nat @-}
-isNat x
-  | x >= 0         = Just x
+------------------------------------------------------------------
+
+-- | Goal
+
+{-@ getPoss  :: [Int] -> [Pos] @-}
+getPoss      = filter isPos
+
+{-@ getEvens :: [Int] -> [Even] @-}
+getEvens     = filter isEven
+
+{-@ getOdds  :: [Int] -> [Odd] @-}
+getOdds      = filter isOdd
+
+-- | `Pos`, `Even` and `Odd` are just subsets of `Int`:
+
+{-@ type Pos  = {v:Int| 0 < v}        @-}
+
+{-@ type Even = {v:Int| v mod 2 == 0} @-}
+
+{-@ type Odd  = {v:Int| v mod 2 /= 0} @-}
+
+
+--------------------------------------------------------------------------
+-- | Take 1: Map, maybe?
+--------------------------------------------------------------------------
+
+filter1          :: (a -> Maybe b) -> [a] -> [b]
+
+filter1 f []     = []
+filter1 f (x:xs) = case f x of
+                     Just y  -> y : filter1 f xs
+                     Nothing ->     filter1 f xs
+
+isPos x
+  | x > 0          = Just x
   | otherwise      = Nothing
 
-{-@ isEven         :: Int -> Maybe Even @-}
-isEven x           
+isEven x
   | x `mod` 2 == 0 = Just x
   | otherwise      = Nothing
 
-{-@ isOdd          :: Int -> Maybe Odd  @-}
 isOdd x
-  | x `mod` 2 == 1 = Just x
+  | x `mod` 2 /= 0 = Just x
   | otherwise      = Nothing
 
------------------------------------------------------------------
--- | Take 1: map, maybe
------------------------------------------------------------------
 
-filter1          :: (a -> Maybe b) -> [a] -> [b]
-filter1 f []     = []
-filter1 f (x:xs) = case f x of
-                     Just y  -> y : filter1 f xs 
-                     Nothing ->     filter1 f xs
-
-{-@ getNats1 :: [Int] -> [Nat] @-}
-getNats1     = filter1 isNat
+{-@ getPoss1 :: [Int] -> [Pos] @-}
+getPoss1     = filter1 isPos
 
 {-@ getEvens1 :: [Int] -> [Even] @-}
 getEvens1    = filter1 isEven
@@ -48,18 +70,20 @@ getEvens1    = filter1 isEven
 {-@ getOdds1 :: [Int] -> [Odd] @-}
 getOdds1     = filter1 isOdd
 
------------------------------------------------------------------
--- | Take 2: single type variable
------------------------------------------------------------------
+
+--------------------------------------------------------------------------
+-- | Take 2: One Type Variable
+--------------------------------------------------------------------------
 
 filter2          :: (a -> Maybe a) -> [a] -> [a]
+
 filter2 f []     = []
 filter2 f (x:xs) = case f x of
-                     Just y  -> y : filter2 f xs 
+                     Just y  -> y : filter2 f xs
                      Nothing ->     filter2 f xs
 
-{-@ getNats2 :: [Int] -> [Nat] @-}
-getNats2     = filter2 isNat
+{-@ getPoss2 :: [Int] -> [Pos] @-}
+getPoss2     = filter2 isPos
 
 {-@ getEvens2 :: [Int] -> [Even] @-}
 getEvens2    = filter2 isEven
@@ -67,22 +91,46 @@ getEvens2    = filter2 isEven
 {-@ getOdds2 :: [Int] -> [Odd] @-}
 getOdds2     = filter2 isOdd
 
------------------------------------------------------------------
--- | Take 3: abstractly refined filter 
------------------------------------------------------------------
+--------------------------------------------------------------------------
+-- | Take 3: Add Abstract Refinement
+--------------------------------------------------------------------------
+
+{-@ filter3      :: forall a <p :: a -> Prop>.
+                      (a -> Maybe a<p>) -> [a] -> [a<p>] @-}
+filter3 f []     = []
+filter3 f (x:xs) = case f x of
+                     Just x'  -> x' : filter3 f xs
+                     Nothing ->       filter3 f xs
+
+
+--  Now, we've **decoupled** the filter-property from the type variable `a`.
+
+-- The input still is a mere `a`, but the output is an `a` with bells on,
+-- specifically, which satisfies the (abstract) refinement `p`.
+
+-- Voila!
+
+
+{-@ getPoss3  :: [Int] -> [Pos] @-}
+getPoss3      = filter3 isPos
+
+{-@ getEvens3 :: [Int] -> [Even] @-}
+getEvens3     = filter3 isEven
+
+{-@ getOdds3  :: [Int] -> [Odd] @-}
+getOdds3      = filter3 isOdd
+
+doubles       = filter3 (\x -> Just (x + x)) 
+
+
+--------------------------------------------------------------------------
 
 {-@ filter      :: forall a <p :: a -> Prop>.
-                     (a -> Maybe a<p>) -> [a] -> [a<p>] @-}
+                       (x:a -> Maybe {v:a<p> | v = x})
+                    -> xs:[a]
+                    -> {v:[a<p>] | Set_sub (elts v) (elts xs)} @-}
+
 filter f []     = []
 filter f (x:xs) = case f x of
-                    Just x'  -> x' : filter f xs 
+                    Just x'  -> x' : filter f xs
                     Nothing ->       filter f xs
-
-{-@ getNats :: [Int] -> [Nat] @-}
-getNats     = filter isNat
-
-{-@ getEvens :: [Int] -> [Even] @-}
-getEvens    = filter isEven
-
-{-@ getOdds :: [Int] -> [Odd] @-}
-getOdds     = filter isOdd
