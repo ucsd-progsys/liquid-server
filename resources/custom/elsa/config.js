7ed19e658ad7daf0d81cacddd9b1e62470acc7a7
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-03-11 12:08:00 +0530

    init elsa server

---
+++ resources/custom/elsa/config.js
@@ -0,0 +1,37 @@
+/*******************************************************************************/
+/************** Configuration Parameters For Server ****************************/
+/*******************************************************************************/
+
+'use strict';
+
+/************** Titles *********************************************************/
+
+var demoTitle     = "Elsa";
+var demoSubtitle  = "An Interactive Lambda Calculus Evaluator";
+
+/************** Header Links 8**************************************************/
+
+var allLinks = [ { url: "https://github.com/ucsd-progsys/elsa" , name: "Code" } 
+               ];
+
+/************** Editor Modes ***************************************************/
+
+var toolName       = "elsa";
+var editorTheme    = "ace/theme/xcode";
+var editorMode     = "ace/mode/haskell";
+var defaultErrText = "Invalid Evaluation";
+ 
+/************** List of Demos **************************************************/
+
+var allCategories = [ { type : "basic"    , name: "Basics" }
+                    ];
+
+var allDemos =
+  { // Basic Demos
+    "blank.lc"              : { name : "Blank"            , type : "basic"  },
+    "alpha.lc"              : { name : "Alpha Conversion" , type : "basic"  },
+    "beta.lc"               : { name : "Beta Reduction"   , type : "basic"  },
+    "bool.lc"               : { name : "Church Booleans"  , type : "basic"  },
+    "nats.lc"               : { name : "Church Numerals"  , type : "basic"  },
+    "compose.lc"            : { name : "Composition"      , type : "basic"  },
+  };
