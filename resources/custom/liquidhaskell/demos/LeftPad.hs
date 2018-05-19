aec4c73b2cb1ee740bf16e29ba7835229b2e7adb
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-05-19 16:33:48 -0700

    convert to .hs

--- resources/custom/liquidhaskell/demos/LeftPad.hs
+++ resources/custom/liquidhaskell/demos/LeftPad.hs
@@ -6,34 +6,20 @@
 module PadLeft where
 
 import Prelude hiding (max, replicate, (++), (!!))
-(!!) :: [a] -> Int -> a
-size :: [a] -> Int
-(++) :: [a] -> [a] -> [a]
-leftPadObviousThm :: Int -> a -> [a] -> ()
-thmReplicate      :: Int -> a -> Int -> ()
-thmAppLeft        :: [a] -> [a] -> Int -> ()
-thmAppRight       :: [a] -> [a] -> Int -> ()
-thmLeftPad        :: Int -> a -> [a] -> Int -> ()
 
-{-@ reflect max @-}
-max :: Int -> Int -> Int
-max x y = if x > y then x else y
-
--- A ghost function only used in the specification
-{-@ leftPadVal :: n:{Int | False} -> _ -> _ -> _ -> _ @-}
-
-
--- Implementation
--- --------------
+-------------------------------------------------------------------------------
+-- | Implementation
+-------------------------------------------------------------------------------
 
 {-@ reflect leftPad @-}
-leftPad :: Int -> a -> [a] -> [a]
+{-@ leftPad :: n:Int -> c:a -> xs:[a] ->
+                {res:[a] | size res = max n (size xs)}
+  @-}
 leftPad n c xs
   | 0 < k     = replicate k c ++ xs
   | otherwise = xs
   where
     k         = n - size xs
-
 
 {-@ measure size @-}
 {-@ size :: [a] -> Nat @-}
@@ -53,28 +39,9 @@ replicate :: Int -> a -> [a]
 replicate 0 _ = []
 replicate n c = c : replicate (n - 1) c
 
-
--- What shall we Prove?
--- --------------------
-
--- My eyes roll whenever I read the phrase "proved X (a function, a program) _correct_".
-
--- There is no such thing as "correct".
-
--- There are only "specifications" or "properties",
--- and proofs that ensures that your code matches
--- those specifications or properties.
-
--- What, _specifications_ shall we verify about our
--- implementation of `leftPad`? One might argue that
--- the above code is "obviously correct", i.e. the
--- implementation more or less directly matches the
--- informal requirements.
-
--- Indeed, one way to formalize this "obviously correct"
--- is to verify a _specification_ that directly captures
--- the informal requirements:
-
+-------------------------------------------------------------------------------
+-- | Proving the "Obviously Correct" Specification 
+-------------------------------------------------------------------------------
 
 {-@ leftPadObviousThm :: n:Int -> c:a -> xs:[a] ->
       { leftPad n c xs = if (size xs < n)
@@ -84,8 +51,9 @@ replicate n c = c : replicate (n - 1) c
 leftPadObviousThm _ _ _ = ()
 
 
--- Reasoning about Sequences
--- -------------------------
+-------------------------------------------------------------------------------
+-- | Reasoning about Sequences
+-------------------------------------------------------------------------------
 
 -- **Indexing into a Sequence**
 
@@ -119,33 +87,13 @@ thmAppLeft (x:xs) ys i = thmAppLeft xs ys (i-1)
 thmAppRight []     ys i = ()
 thmAppRight (x:xs) ys i = thmAppRight xs ys (i-1)
 
-
--- Proving Hillel's Specifications
--- -------------------------------
-
--- **Size Specification**
-
-{-@ leftPad :: n:Int -> c:a -> xs:[a] ->
-                {res:[a] | size res = max n (size xs)}
-  @-}
-
-
--- **Pad-Value Specification**
-
+-------------------------------------------------------------------------------
+-- | Proving Hillel's Specification
+-------------------------------------------------------------------------------
 {-@ thmLeftPad
       :: n:_ -> c:_ -> xs:{size xs < n} -> i:{Nat | i < n} ->
-         { leftPad n c xs !! i ==  leftPadVal n c xs i }
+         { (leftPad n c xs !! i) == (if (i < n - size xs) then c else (xs !! (i - (n - size xs)))) }
   @-}
-
-{-@ reflect leftPadVal @-}
-leftPadVal n c xs i
-  | i < k     = c
-  | otherwise = xs !! (i - k)
-  where k     = n - size xs
-
-
--- **Pad-Value Verification**
-
 thmLeftPad n c xs i
   | i < k     = thmAppLeft  cs xs i `seq` thmReplicate k c i
   | otherwise = thmAppRight cs xs i
@@ -153,18 +101,21 @@ thmLeftPad n c xs i
     k         = n - size xs
     cs        = replicate k c
 
+-------------------------------------------------------------------------------
+-- | Haskell sigs moved to avoid clutter 
+-------------------------------------------------------------------------------
 
+(!!)              :: [a] -> Int -> a
+size              :: [a] -> Int
+(++)              :: [a] -> [a] -> [a]
+leftPadObviousThm :: Int -> a -> [a] -> ()
+thmReplicate      :: Int -> a -> Int -> ()
+thmAppLeft        :: [a] -> [a] -> Int -> ()
+thmAppRight       :: [a] -> [a] -> Int -> ()
+thmLeftPad        :: Int -> a -> [a] -> Int -> ()
+leftPad           :: Int -> a -> [a] -> [a]
 
+{-@ reflect max @-}
+max :: Int -> Int -> Int
+max x y = if x > y then x else y
 
-
-
--- LINKS
---
--- [demo]:             http://goto.ucsd.edu:8090/index.html#?demo=LeftPad.hs
--- [dafny-leftpad]:    https://rise4fun.com/Dafny/nbNTl
--- [spark-leftpad]:    https://blog.adacore.com/taking-on-a-challenge-in-spark
--- [fstar-leftpad]:    https://gist.github.com/graydon/901f98049d05db65d9a50f741c7f7626
--- [idris-leftpad]:    https://github.com/hwayne/lets-prove-leftpad/blob/master/idris/Leftpad.idr
--- [dafny-seq-axioms]: https://github.com/Microsoft/dafny/blob/master/Binaries/DafnyPrelude.bpl#L898-L1110
--- [tag-reflection]:   /tags/reflection.html
--- [tag-ple]:          /tags/ple.html
