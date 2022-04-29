f60b1ad03f32fa9fd89b68535d9dfce02e4a487c
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-04-29 13:35:36 -0700

    asd

---
+++ resources/custom/flux/demos/inc.rs
@@ -0,0 +1,35 @@
+#![feature(register_tool)]
+#![register_tool(lr)]
+
+// Increment via a value
+#[lr::sig(fn(bool[true]) -> i32)]
+pub fn assert(_b: bool) -> i32 {
+    0
+}
+
+// Increment via a value
+#[lr::sig(fn(n: i32) -> i32[n+1])]
+pub fn inc_val(n: i32) -> i32 {
+    n + 1
+}
+
+
+// Increment via a strong reference
+#[lr::sig(fn(x: &strg i32[@n]) -> i32 ensures x: i32[n+1])]
+pub fn inc_ref(x: &mut i32) -> i32 {
+    *x += 1;
+    0
+}
+
+// Test 
+fn _test() -> i32 { 
+   
+    // client of inc_val
+    assert(inc_val(10) == 11);
+
+    // client of inc_ptr
+    let mut p = 10;
+    inc_ref(&mut p);
+    assert(p == 11);
+    0
+}
