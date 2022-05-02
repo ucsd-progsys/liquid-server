9d85bafb202cdb51c7a003dbfccf84da1418087e
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-05-02 11:35:17 -0700

    update

--- resources/custom/flux/demos/vectors.rs
+++ resources/custom/flux/demos/vectors.rs
@@ -10,30 +10,31 @@ use rvec::RVec;
 #[lr::sig(fn(n:i32{0 <= n}, val:i32) -> RVec<i32>[n])]
 pub fn init(n:i32, val:i32) -> RVec<i32> {
     let mut i = 0;
-    let res = RVec::new();
+    let mut res = RVec::new();
     while i < n {
         res.push(val);
+        i += 1;
     }
     res
 }
 
 // Reading vectors ----------------------------------------------
 
-#[lr::sig(fn(&mut RVec<i32>[@n]) -> i32[0] requires 2 <= n)]
+#[lr::sig(fn(&mut RVec<i32>[@n]) -> i32 requires 2 <= n)]
 pub fn sum2(vec: &mut RVec<i32>) -> i32 {
     let v0 = *vec.get(0);
     let v1 = *vec.get(1);
     v0 + v1
 }
 
-#[lr::sig(fn(&mut RVec<i32{v:0 <= v}>) -> i32{v:0 <= v})]
-pub fn sum(vec: &mut RVec<i32>) -> i32 {
+#[lr::sig(fn(&RVec<i32{v:0 <= v}>) -> i32{v:0 <= v})]
+pub fn sum(vec: &RVec<i32>) -> i32 {
     let n = vec.len();
     let mut i = 0;
     let mut total = 0;
     while i < n {
         total += *vec.get(i);
+        i += 1;
     }
     total
 }
-
